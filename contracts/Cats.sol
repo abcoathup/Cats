f45b41ac0c735c631b630a825b6b514d37f927e3
Andrew B Coathup <28278242+abcoathup@users.noreply.github.com>
2018-05-28 12:37:07 +1000

    Restrict cat minting

--- contracts/Cats.sol
+++ contracts/Cats.sol
@@ -1,24 +1,35 @@
-pragma solidity 0.4.23;
+pragma solidity 0.4.24;
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 
 /**
  * @title Cats NFT
  * @dev ERC721
  */
-contract Cats is ERC721Token {
+contract Cats is ERC721Token, Ownable {
     constructor() public 
     
     ERC721Token("Cats", "CAT")
     { 
     }
 
-    function mint(address _to, uint256 _tokenId) public {
-        super._mint(_to, _tokenId);
+    function mint(address _to, string _tokenURI) public onlyOwner {
+        uint256 newTokenId = _getNextTokenId();
+        super._mint(_to, newTokenId);
+        super._setTokenURI(newTokenId, _tokenURI);
     }
 
-    function setTokenURI(uint256 _tokenId, string _uri) public {
+    function setTokenURI(uint256 _tokenId, string _uri) public onlyOwner {
         super._setTokenURI(_tokenId, _uri);
+    }
+
+    /**
+    * @dev calculates the next token ID based on totalSupply
+    * @return uint256 for the next token ID
+    */
+    function _getNextTokenId() private view returns (uint256) {
+        return totalSupply().add(1); 
     }
 }
